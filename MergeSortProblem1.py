997cee21de41c521449b58b9d3c30c17b08a861f
Christian Stewart <stu@gateway.masano.com>
2021-07-16 10:53:12 -0400

    Descriptions

--- MergeSortProblem1.py
+++ MergeSortProblem1.py
@@ -1,1 +1,12 @@
-print("hello world")
+# Problem Description
+# Merge two sorted arrays of integers into a single sorted array
+# Input
+# Two lines, each line describes a sorted array. The first number in the line gives n, the number of integers in the array, 
+# and the following n numbers give the n integers in the array, in ascending order. All numbers are separated by space.
+# You can assume that 0 <= n <= 100000, and that each number in the arrarys are in the range of [-2147483648, 2147483647].
+
+# Output
+# One line, describing the merged sorted array, in ascending order. 
+# The first integer should be the total number of integers in the merged array, 
+# and the following integer(s) should give all numbers in the merged array (in ascending order). 
+# All numbers should be separated with space.
